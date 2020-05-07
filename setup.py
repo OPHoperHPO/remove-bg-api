4d0ee03c413648cd682b3122c9338e8a55fdc8ba
Anodev <29470622+ophoperhpo@users.noreply.github.com>
2020-05-08 04:18:17 +1000

    😀First fix!😀

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 """Setup script"""
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-
+from setuptools import find_packages
+from distutils.core import setup
 
 with open('README.md') as f:
     readme = f.read()
@@ -16,9 +16,20 @@ setup(
     description='Remove.bg Python API Wrapper',
     long_description=readme,
     author='Anodev (OPHoperHPO)',
+    keywords = ['removebg.py', 'removebg', 'api', "wrapper"],
     author_email='netbook21@mail.ru',
     url='https://github.com/OPHoperHPO/removebg.py',
     license=lic,
     long_description_content_type='text/markdown',
-    packages=find_packages(exclude=('docs', 'examples'))
+    packages=find_packages(exclude=('docs', 'examples')),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
